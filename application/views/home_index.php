5c182c6f86048a0a038584e31aec141863759a02
Steven Dumbura <stevedumbura@gmail.com>
2014-08-30 16:13:28 +0100

    landing page concept

--- application/views/home_index.php
+++ application/views/home_index.php
@@ -2,21 +2,36 @@
 	require_once('header.php');
 ?>
 
-<div class="img">
+
+
+<div class="slide1" id="intro">
+
 <div class="row">
-  <div class="12-columns large">
-      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-      <div class="row">
-        <div class="12-columns large">
-          <p align="center">
-          <span class="panel">Welcome to EventZ</span><br/><br/>
-          <button class="button round">Proceed</button>
-          </p>
-        </div>
-      </div>
+  <div class="large-4 columns left">
+
+          <div class="section">
+            <h4 align="left">Welcome to EventZ.</h4>
+          </div>
+
+
+            <h3  align="left">Find events.</h3>
+            <h3 align="left">Share events.</h3>
+            <h3 align="left">Create events.</h3>
+
   </div>
+  <div class="large-2 columns center">
+    &nbsp;
+  </div>
+  <div class="large-6 columns right">
+    &nbsp;
+  </div>
+
 </div>
+
 </div>
+
+
+
 
 <?php
 	require_once('footer.php');
